e7fb0adf0a6fa560718e3a2698d446b2eee8215a
DevriesL <therkduan@gmail.com>
2019-03-13 23:41:49 +0800

    WIP: drm: Hijack backlight update function
    Change-Id: Ie78e70bc518a4dfd8dcdf3ac85cc1ccbed3cbcd3
    Signed-off-by: DevriesL <therkduan@gmail.com>
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -34,6 +34,8 @@
 #include "dsi_pwr.h"
 #include "sde_dbg.h"
 
+#include "exposure_adjustment.h"
+
 #define to_dsi_display(x) container_of(x, struct dsi_display, host)
 #define INT_BASE_10 10
 #define NO_OVERRIDE -1
@@ -167,6 +169,10 @@ int dsi_display_set_backlight(void *display, u32 bl_lvl)
 
 	bl_scale_ad = panel->bl_config.bl_scale_ad;
 	//bl_temp = (u32)bl_temp * bl_scale_ad / MAX_AD_BL_SCALE_LEVEL;
+
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	bl_temp = ea_panel_calc_backlight(bl_temp);
+#endif
 
 	pr_debug("bl_scale = %u, bl_scale_ad = %u, bl_lvl = %u\n",
 		bl_scale, bl_scale_ad, (u32)bl_temp);
