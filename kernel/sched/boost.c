86330e5c4e4a3271a4b0b7d593cc6b5a2dfeeefb
0ctobot <return.of.octobot@gmail.com>
2019-03-30 19:45:29 -0400

    sched/boost: Disable sched_boost while preserving node functionality
    In d99931a ("sched/boost: Boost CPU and DDR bus when sched_boost is
    triggered") we hooked into sched_boost to trigger boost kicks when a
    nonzero value is written to /proc/sys/kernel/sched_boost. This was
    effective but also introduced some redundancies as a SchedTune boost is
    inherently invoked when an input boost kick is called for. Furthermore,
    as per kerneltoast/android_kernel_google_bluecross@3d72695 and in light
    of reintroducing a dedicated frame boost function, we will render
    sched_boost non-operational while preserving userspace ability to call
    for boost kicks by returning early from _sched_set_boost after the node
    is written to.
    
    [0ctobot: This reverts commit cce32d2 ("sched/boost: Perform SchedTune boosting when sched_boost is triggered")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    
    Change-Id: Ibe296f3418655e75489780075b0b0ae15a80386d

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -118,10 +118,6 @@ enum sched_boost_policy sched_boost_policy(void)
 	return boost_policy;
 }
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int boost_slot;
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
-
 static bool verify_boost_params(int old_val, int new_val)
 {
 	/*
@@ -134,14 +130,13 @@ static bool verify_boost_params(int old_val, int new_val)
 
 static void _sched_set_boost(int old_val, int type)
 {
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (type > 0) {
-		do_stune_sched_boost("top-app", &boost_slot);
 		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-	} else
-		reset_stune_boost("top-app", boost_slot);
-#endif // CONFIG_DYNAMIC_STUNE_BOOST
+	}
+
+	if (IS_ENABLED(CONFIG_CPU_INPUT_BOOST) || IS_ENABLED(CONFIG_DEVFREQ_BOOST))
+		return;
 
 	switch (type) {
 	case NO_BOOST:
