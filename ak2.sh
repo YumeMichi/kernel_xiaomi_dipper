09b1a855cae9247ced54349a08e66f8e43bae2e2
YumeMichi <do4suki@gmail.com>
2019-04-21 13:04:07 +0800

    Polar Kernel: Bump version for FakeDC
    Change-Id: Ic166dd780dc661a66bb9919e709210760e136819
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- ak2.sh
+++ ak2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+VERSION="$(cat version)-FakeDC-$(date +%F | sed s@-@@g)"
 
 if [ -e out/arch/arm64/boot/Image.gz-dtb ] ; then
     # Pack AnyKernel2
