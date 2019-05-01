098b6ac02c4cf08df7e4441c0c9952f30c6d2935
ethan-halsall <ethan.halsall@gmail.com>
2019-05-01 22:32:32 +0100

    init: Tweak cfq
    Change-Id: I6bf64057c99e090faf69a4705de3c3cefed4f261
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- anykernel2/95-polar.sh
+++ anykernel2/95-polar.sh
@@ -36,5 +36,36 @@ sleep 2
 # Apply overrides and tweaks
 #
 
-echo 85 > /proc/sys/vm/swappiness # Reduce kswapd cpu usage
-echo 1 > /sys/module/printk/parameters/console_suspend # Marginally reduce suspend latency
+# Reduce kswapd cpu usage
+echo 85 > /proc/sys/vm/swappiness
+
+# Marginally reduce suspend latency
+echo 1 > /sys/module/printk/parameters/console_suspend
+
+# IO block tweaks for better system performance;
+for i in /sys/block/*/queue; do
+  echo 0 > $i/add_random;
+  echo 0 > $i/iostats;
+  echo 0 > $i/nomerges;
+  echo 32 > $i/nr_requests;
+  echo 128 > $i/read_ahead_kb;
+  echo 0 > $i/rotational;
+  echo 1 > $i/rq_affinity;
+  echo "cfq" > $i/scheduler;
+done;
+
+# Tweak cfq IO scheduler for less latency
+for i in /sys/block/*/queue/iosched; do
+  echo 4 > $i/quantum;
+  echo 80 > $i/fifo_expire_sync;
+  echo 330 > $i/fifo_expire_async;
+  echo 12582912 > $i/back_seek_max;
+  echo 1 > $i/back_seek_penalty;
+  echo 60 > $i/slice_sync;
+  echo 50 > $i/slice_async;
+  echo 2 > $i/slice_async_rq;
+  echo 0 > $i/slice_idle;
+  echo 0 > $i/group_idle;
+  echo 1 > $i/low_latency;
+  echo 300 > $i/target_latency;
+done;
